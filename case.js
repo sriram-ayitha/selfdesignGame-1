449b6455160cd3a7f847f41dc40ccfae1fe46298
sriram-ayitha <83587499+sriram-ayitha@users.noreply.github.com>
2021-08-24 17:57:58 +0530

    Add files via upload

--- case.js
+++ case.js
@@ -2,17 +2,20 @@ class Case{
  constructor(x,y){
     this.box = createSprite(x,y);
     this.box.addImage("caseclosed",caseclosed) ;
-    this.box.scale = 0.3;
+    this.box.scale = 0.2;
     this.touched = 0;
 
 }
 casechange(caseSprite){
 
+  if(this.touched === 0){
+    this.touched = 1;
     var r = Math.round(random(1,2))
     console.log(r);
     switch (r){
       case 1 : caseSprite.addImage("caseopen",caseopened);
                caseSprite.changeImage("caseopen",caseopened);
+               score = score+1;
             break;
       case 2 : caseSprite.addImage("caseempty",caseempty);
                caseSprite.changeImage("caseempty",caseempty);
@@ -21,4 +24,7 @@ casechange(caseSprite){
   
     } 
   }
+   
+  }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,12 +1,8 @@
 var boy,boyImg;
-var case1,case2,case3,case4,case5;
-var case1touched = 0;
-var case2touched = 0;
-var case3touched = 0;
-var case4touched = 0;
-var case5touched = 0;
-var case6touched = 0;
-var case7touched = 0;
+var case1,case2,case3,case4,case5,case6,case7,case8;
+var gameState;
+var fish;
+var score = 0;
 
 function preload(){
 
@@ -14,55 +10,50 @@ function preload(){
   caseopened = loadImage("treasurechestfull.jpg");
   caseempty  = loadImage("treasurechestsempty.jpg");
   boyImg = loadImage("boy.jpg");
+  
+  fishImg = loadImage("fishimage.jpeg");
+ 
 }
 
 function setup(){
-  createCanvas(600,600)
+  createCanvas(600,650);
+  background("blue");
+  
+  
  
 
  boy = createSprite(25,100,20,20);
  boy.addImage("case",boyImg);
  boy.scale = 0.2;
 
- case1 = createSprite(100,100,10,10);
- case1.addImage("case",caseclosed);
- case1.scale = 0.2; 
- case2 = createSprite(100,180,10,10);
- case2.addImage("case",caseclosed);
- case2.scale = 0.2;  
- case3 = createSprite(100,260,10,10);
- case3.addImage("case",caseclosed);
- case3.scale = 0.2; 
- case4 = createSprite(100,340,10,10);
- case4.addImage("case",caseclosed);
- case4.scale = 0.2;                                                                    
- case5 = createSprite(100,420,10,10);
- case5.addImage("case",caseclosed);
- case5.scale = 0.2; ;  
- case6 = createSprite(100,500,10,10);
- case6.addImage("case",caseclosed);
- case6.scale = 0.2; 
- case7 = createSprite(100,580,10,10);
- case7.addImage("case",caseclosed);
- case7.scale = 0.2; 
- case8 = createSprite(250,100,10,10);
- case8.shapeColor = "yellow";
- case9 = createSprite(250,180,10,10);
- case9.shapeColor = "green";
- case10 = createSprite(250,260,10,10);
- case10.shapeColor = "yellow"; 
- case11 = createSprite(250,340,10,10);
- case11.shapeColor = "green";
- case12 = createSprite(250,420,10,10);
- case12.shapeColor = "yellow";
- case13 = createSprite(250,500,10,10);
- case13.shapeColor = "green";
- case14 = createSprite(250,580,10,10);
- case14.shapeColor = "yellow";
+ gameState = "play";
+ 
+
+ case1 = new Case(Math.round(random(100,500)),120);
+ 
+ 
+ case2 = new Case(Math.round(random(100,500)),180); 
+ case3 = new Case(Math.round(random(100,500)),260); 
+ case4 = new Case(Math.round(random(100,500)),340);                                                                  
+ case5 = new Case(Math.round(random(100,500)),420);  
+ case6 = new Case(Math.round(random(100,500)),500);
+ case7 = new Case(Math.round(random(100,500)),580);
+ case8 = new Case(Math.round(random(100,500)),620);
+
+ fishGroup = new Group();
 }
 
 function draw(){
- background("white");
+ background("White");
+ edges = createEdgeSprites();
+ bg_gif = loadImage("bg.gif");
+
+ if(gameState === "play"){
+    
+ textSize(15);
+ text("Score: "+score,500,25);
+ textcolor = blue;
+
 
  if(keyDown(LEFT_ARROW)){
    boy.x = boy.x-10;
@@ -80,65 +71,99 @@ if(keyDown(UP_ARROW)){
   boy.y = boy.y-10;
 }
 
-if(boy.isTouching(case1) && case1touched===0){
-  case1touched = 1;
-  casechange(case1);
-
+if(boy.isTouching(case1.box)){
+  case1.casechange(case1.box);
 }
 
-if(boy.isTouching(case2) && case2touched===0){
- case2touched = 1;
- casechange(case2);
+if(boy.isTouching(case2.box)){
+  case2.casechange(case2.box);
 }
 
-if(boy.isTouching(case3) && case3touched===0){
-  case3touched = 1;
-  casechange(case3);
+if(boy.isTouching(case3.box)){
+  case3.casechange(case3.box);
+}
+
+if(boy.isTouching(case4.box)){
+  case4.casechange(case4.box);
+}
+
+if(boy.isTouching(case5.box)){
+  case5.casechange(case5.box);
+}
+
+if(boy.isTouching(case6.box)){
+  case6.casechange(case6.box);
+}
+
+if(boy.isTouching(case7.box)){
+  case7.casechange(case7.box);
+}
+
+if(boy.isTouching(case8.box)){
+  case8.casechange(case8.box);
+}
+
+if(score ===3){
+  textSize(30);
+  text("CONGRATULATIONS",300,300);
+  gameState = "nextlevel"
+  vault();           
+}
+
+if(keyDown('space') && gameState === 'nextlevel'){
+
+console.log('space');
+  
+}
+
+
+spawnFish();
+if(fishGroup.isTouching(boy)){
+  gameState = "end"
+}
+
+boy.collide(case8.box);
+boy.collide(case7.box);
+boy.collide(case6.box);
+boy.collide(case5.box);
+boy.collide(case4.box);
+boy.collide(case3.box);
+boy.collide(case2.box);
+boy.collide(case1.box);
+
+drawSprites();
+
+}
+if(gameState === "end"){
+  background("red");
+  textSize(20)
+  textColor = "white"
+  text("Game Over",200,250);
+}
+}
+
+function spawnFish(){
+
+ if(frameCount% 60===0){
+  var fish = createSprite(100,0,50,50);
+  fish.x = Math.round(random(120,500));
+  fish.velocityY = 3;
+  fishGroup.add(fish);
+  fish.addImage("image",fishImg);
+  fish.scale = 0.3;
  }
+  
+  
 
- if(boy.isTouching(case4) && case4touched===0){
-  case4touched = 1;
-  casechange(case4);
- }
-
- if(boy.isTouching(case5) && case5touched===0){
-  case5touched = 1;
-  casechange(case5);
- }
-
- if(boy.isTouching(case6) && case6touched===0){
-  case6touched = 1;
-  casechange(case6);
- }
-
- if(boy.isTouching(case7) && case7touched===0){
-  case7touched = 1;
-  casechange(case7);
 }
-boy.collide(case7);
-boy.collide(case6);
-boy.collide(case5);
-boy.collide(case4);
-boy.collide(case3);
-boy.collide(case2);
-boy.collide(case1);
-createEdgeSprites();
-  drawSprites();
+function vault(){
+  background("green");
+  boy.x = 100;
+  var demon = createSprite(300,300,20,20);
+  demon.velocityY = 10;
+  
+  demon.bounceOff(edges);x
+  
+  var treasure = createSprite(350,200,20,20);
+  
 }
-
-function casechange(caseSprite){
-
-  var r = Math.round(random(1,2))
-  console.log(r);
-  switch (r){
-    case 1 : caseSprite.addImage("caseopen",caseopened);
-             caseSprite.changeImage("caseopen",caseopened);
-          break;
-    case 2 : caseSprite.addImage("caseempty",caseempty);
-             caseSprite.changeImage("caseempty",caseempty);
-            break;
-    default : break;               
-
-  } 
-}
-
